5313019f0ab82e465b8854fdb5ffc13451dac0c4
Ojochogwu Dickson <07afrik@gmail.com>
2023-04-19 22:45:08 +0000

    render

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from "./Button";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomButton };
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
