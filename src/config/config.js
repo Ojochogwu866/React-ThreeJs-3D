427ea3094ba7c828cb9dc8815757e82ed229a7d9
Ojochogwu Dickson <07afrik@gmail.com>
2023-04-21 01:50:23 +0000

    drv

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,8 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/mystic",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "",
   },
 };
 
 export default config;
-//ec2-13-50-241-92.eu-north-1.compute.amazonaws.com:3000
